3a86a9d806562eb891ce40b189d39d979e4803aa
Matheus Marcelo <matheusmarcelo314@gmail.com>
2024-02-22 23:03:15 -0300

    JWT lib installation for user authentication

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using DevExplorerAPI.DevExplorer.Repositories.UserRepository;
 using DevExplorerAPI.DevExplorer.Services.UserService;
 using DevExplorerAPI.DevExplorer.Interfaces.IUserService;
 using DevExplorerAPI.DevExplorer.Models.DapperContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,28 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["JWT:issuer"],
+            ValidAudience = builder.Configuration["JWT:audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:secretKey"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    }
+);
 
 var app = builder.Build();
 
